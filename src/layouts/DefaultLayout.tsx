eae5652396f1922b2ed6fab2307eae34c379d996
Nzii3 <exeyst@gmail.com>
2024-02-10 21:33:05 -0800

    Update DefaultLayout.tsx

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -19,10 +19,10 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ title, description, child
         <>
             <Head>
                 <title>{ title } - { siteConfig.tagline }</title>
-                <meta name="description" content="Introducing Sentry: Your Multipurpose bot! Advanced commands, custom messages, custom punishments, applications, and customizable whitelists for enhanced control. Elevate your community with ease." />
-                <meta property="og:description" content="Introducing Sentry: Your Multipurpose bot! Advanced commands, custom messages, custom punishments, applications, and customizable whitelists for enhanced control. Elevate your community with ease." />
+                <meta name="description" content="Imagine a custom bot made to your needs at a very cheap price..." />
+                <meta property="og:description" content="Imagine a custom bot made to your needs at a very cheap price..." />
                 <meta property="og:title" content={ title } />
-                <meta name="theme-color" content="#5865f2" />
+                <meta name="theme-color" content="#350ad2" />
             </Head>
             <Layout>
                 { children }
